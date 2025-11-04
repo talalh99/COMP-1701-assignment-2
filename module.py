06b78c7c67e0e0deed9589b428e20616835211b7
talalh99 <talalh433@gmail.com>
2025-11-04 11:14:19 -0700

    added in code skeleton

---
+++ module.py
@@ -0,0 +1,19 @@
+# function that returns the mean from any length list of numerical data
+
+
+# A function that returns the median from any length list of numerical data.
+
+
+# A function that when passed a list of locations: 
+
+# a) asks the user to enter the current air quality for each location, 
+# b) adds that numerical value to a list, and then 
+# c) returns a list of those entered values.
+
+
+#A function that when passed a list of numbers and a parallel list of locations, returns a list of the names of the two locations with the worst (highest) values.
+
+
+
+#A function that when passed a list of numbers, tallies the number of items that would qualify as ‘Good’ (i.e. <= 50) and returns that tally as a percentage of data locations.
+
